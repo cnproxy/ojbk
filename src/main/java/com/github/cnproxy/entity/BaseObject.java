ce8cd8a11ae65e1051d162414622b00a52486ab0
cnproxy <freedomain888@gmail.com>
2018-06-15 01:51:48 +0800

    null 字段不返回

--- src/main/java/com/github/cnproxy/entity/BaseObject.java
+++ src/main/java/com/github/cnproxy/entity/BaseObject.java
@@ -1,5 +1,6 @@
 package com.github.cnproxy.entity;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,6 +14,7 @@ import java.util.Date;
  */
 @Getter @Setter @ToString
 @EqualsAndHashCode
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public abstract class BaseObject<T> implements Serializable {
 
     private static final long serialVersionUID = 6572642668149697331L;
