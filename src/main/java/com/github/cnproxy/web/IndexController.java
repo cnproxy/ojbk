9c914f27f7dcf733f85ae797fd38fd456d390fc3
cnproxy <freedomain888@gmail.com>
2018-06-15 02:09:45 +0800

    添加登录页面

--- src/main/java/com/github/cnproxy/web/IndexController.java
+++ src/main/java/com/github/cnproxy/web/IndexController.java
@@ -1,4 +1,15 @@
 package com.github.cnproxy.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
 public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "login";
+    }
 }
