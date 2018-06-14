b75dba1bccd095f57a4a1b540cd95efdc6ecd40b
cnproxy <freedomain888@gmail.com>
2018-06-14 14:25:14 +0800

    测试数据回滚

--- src/test/java/com/github/cnproxy/service/UserServiceTest.java
+++ src/test/java/com/github/cnproxy/service/UserServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +22,7 @@ public class UserServiceTest {
     private UserService userService;
 
     @Test
+    @Rollback
     public void saveUser() throws Exception {
         User user = new User();
         user.setQq("10001");
