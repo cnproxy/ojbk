4ef0d1eb2861c36ac63b83ef539f85248089e865
cnproxy <freedomain888@gmail.com>
2018-06-28 18:57:57 +0800

    -- 移除阿里云 maven 仓库 -- fixed sql query mapping to object error

--- src/main/java/com/github/cnproxy/dto/ExpiredRankingDTO.java
+++ src/main/java/com/github/cnproxy/dto/ExpiredRankingDTO.java
@@ -1,5 +1,6 @@
 package com.github.cnproxy.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,6 +14,7 @@ import java.util.Date;
  */
 @Getter @Setter @ToString()
 @EqualsAndHashCode()
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ExpiredRankingDTO implements java.io.Serializable {
 
     private String vmessUUID;
