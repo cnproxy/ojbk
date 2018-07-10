0e2a530c059f803c61e451a583fc9d63326b9a79
cnproxy <freedomain888@gmail.com>
2018-07-10 21:55:21 +0800

    [UPDATE] 修改安全过滤路径

--- src/main/java/com/github/cnproxy/api/TokenController.java
+++ src/main/java/com/github/cnproxy/api/TokenController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/token")
+@RequestMapping("/api/token")
 public class TokenController {
 
     @Autowired
